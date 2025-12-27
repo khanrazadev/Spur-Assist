5f6d400c7cc880ddc6fc879b7fbc12ef65c344b2
Raza Khan <razakhanha05@gmail.com>
2025-12-27 21:13:31 +0530

    updated client url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,14 @@ import { chatHistoryRoutes } from "./routes/chat.history.route.js";
 
 export const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://spur-assist.vercel.app"
+    ],
+  })
+);
 app.use(express.json());
 
 app.use("/api/chat", chatRoutes);
